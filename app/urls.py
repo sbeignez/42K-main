7685001af6bc4cb21a415479325bd9b53fc4e7d4
Michael Dauner <michael@dauner.io>
2015-03-10 23:19:20 +0200

    added runner login site

--- app/urls.py
+++ app/urls.py
@@ -2,4 +2,5 @@ from django.conf.urls import patterns, url
 
 urlpatterns = patterns('app.views',
     url(r'^$', 'home', name='home'),
+    url(r'^runner/login/$', 'runner_login', name='runner_login'),
 )
--- app/views.py
+++ app/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'app/home.html')
+
+def runner_login(request):
+    return render(request, 'app/login-runner.html')
