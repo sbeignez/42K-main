f9e322b72866a2ec1b9fc77495d6cded9c86dbdf
Michael Dauner <michael@dauner.io>
2015-03-10 02:12:28 +0200

    switch to postgresql

--- forty_two_k/settings.py
+++ forty_two_k/settings.py
@@ -66,8 +66,12 @@ WSGI_APPLICATION = 'forty_two_k.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'forty_two_k',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
