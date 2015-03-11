f88968bb319ebae196814866d0c5e5c25195fc6f
Leo <leo.paul.grant@gmail.com>
2015-03-12 01:16:46 +0800

    Tagger page
    - link in urls
    - loop over races

--- app/urls.py
+++ app/urls.py
@@ -1,9 +1,12 @@
 from django.conf.urls import patterns, url
 
+from app import views
+
 urlpatterns = patterns('app.views',
     url(r'^$', 'home', name='home'),
     url(r'^runner/$', 'runner', name='runner'),
     url(r'^photographer/$', 'photographer', name='photographer'),
-    url(r'^tagger/$', 'tagger', name='tagger'),
+    url(r'^tagger/$', views.TaggerView.as_view(), name='tagger'),
+
 
 )
--- app/views.py
+++ app/views.py
@@ -1,5 +1,8 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.views import generic
+
+from app.models import RaceEvent
 
 def home(request):
     return render(request, 'app/home.html')
@@ -7,8 +10,12 @@ def home(request):
 def runner(request):
     return render(request, 'app/runner.html')
 
-def tagger(request):
-    return render(request, 'app/tagger.html')
+class TaggerView(generic.ListView):
+    template_name = 'app/tagger.html'
+    context_object_name = 'races'
+
+    def get_queryset(self):
+        return RaceEvent.objects.all()
 
 def photographer(request):
     return HttpResponse("TODO")
