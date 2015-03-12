7d98f8956966df7a779c8ec37feefea8659ad165
Michael Dauner <michael@dauner.io>
2015-03-13 00:12:50 +0200

    added photographer page

--- app/urls.py
+++ app/urls.py
@@ -4,7 +4,7 @@ from app import views
 
 urlpatterns = patterns('app.views',
     url(r'^$', 'home', name='home'),
-    url(r'^runner/$', 'runner', name='runner'),
+    url(r'^runner/$', views.RunnerView.as_view(), name='runner'),
     url(r'^photographer/$', 'photographer', name='photographer'),
     url(r'^tagger/$', views.TaggerView.as_view(), name='tagger'),
 
--- app/views.py
+++ app/views.py
@@ -7,8 +7,12 @@ from app.models import RaceEvent
 def home(request):
     return render(request, 'app/home.html')
 
-def runner(request):
-    return render(request, 'app/runner.html')
+class RunnerView(generic.ListView):
+    template_name = 'app/runner.html'
+    context_object_name = 'races'
+
+    def get_queryset(self):
+        return RaceEvent.objects.all()
 
 class TaggerView(generic.ListView):
     template_name = 'app/tagger.html'
@@ -18,4 +22,4 @@ class TaggerView(generic.ListView):
         return RaceEvent.objects.all()
 
 def photographer(request):
-    return HttpResponse("TODO")
+    return render(request, 'app/photographer.html')
