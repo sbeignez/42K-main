119ec98cd823bc5e506e100d1475495cf56b9916
Leo <leo.paul.grant@gmail.com>
2015-04-02 04:37:56 +0800

    test FB app credential

--- app/management/commands/createfacebook.py
+++ app/management/commands/createfacebook.py
@@ -1,11 +1,20 @@
 from allauth.socialaccount.models import SocialApp
 from django.contrib.sites.models import Site
 from django.core.management.base import BaseCommand
+from django.conf import settings
 
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
         if not SocialApp.objects.filter(provider="facebook").exists():
-            app = SocialApp.objects.create(provider='facebook', name='Facebook', client_id='1582734425306832',
-                                           secret='3de82319a1b1c2f37a23a7686935aa1e')
+
+            if settings.ENV == "test":
+                app = SocialApp.objects.create(provider='facebook', name='Facebook',
+                                               client_id='1586347994945475',
+                                               secret='9f7ba3a75cd94cb947ab9f56cbb00068')
+            else:
+                app = SocialApp.objects.create(provider='facebook', name='Facebook',
+                                               client_id='1582734425306832',
+                                               secret='3de82319a1b1c2f37a23a7686935aa1e')
+
             app.sites.add(Site.objects.get_current())
